785b4e5f71c8ff50cdb4aa5219255942a3de1389
nein09 <nein09@nein09.com>
2016-02-04 15:29:07 -0800

    Add past event photos to static/img/photos/

---
+++ disasterinfosite/static/img/photos/flood/Clark Fork River flood in Missoula __ Archival Photographs from the University of Montana_files/siteStructureMMPTop.js
@@ -0,0 +1,12 @@
+﻿// MMP Top Site Structure
+// Montana State Library
+// Stacy Bruhn
+// 1/20/2016
+// Top load javascript for MMP Site
+
+var searchSiteString = window.location.href.toLowerCase();
+if (searchSiteString == "http://mtmemory.org/") {
+    window.location.href = "http://montanamemory.org/";
+}
+
+$("body").prepend("<div class='spinnerDiv'><div class='spinnerDivInner'><center><img src='http://mslsrc.mt.gov/images/banners/montana_memory_projectSpinner.jpg' /></center></div></div>")
