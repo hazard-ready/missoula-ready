6ba34c60fe98849cbf6bc576e58a35369432f131
Eldan Goldenberg <eldang@gmail.com>
2016-02-04 16:59:04 -0800

    more compact notation
    consistent with existing work

--- disasterinfosite/static/js/app.js
+++ disasterinfosite/static/js/app.js
@@ -30,8 +30,7 @@ $( document ).ready(function() {
   var layer = new L.TileLayer(osmUrl, {attribution: osmAttrib}).addTo(map);
   layer.setOpacity(0.6);
   
-  var boundary = new L.GeoJSON.AJAX("static/img/boundary.geojson");
-  boundary.addTo(map);
+  var boundary = new L.GeoJSON.AJAX("static/img/boundary.geojson").addTo(map);
 
   document.getElementById('map').style.cursor='default';
   if (query_lat && query_lng) {
