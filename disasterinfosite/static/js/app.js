b17faa8e16124ea20d4b7b83ac8d3a98966cd96a
Eldan Goldenberg <eldang@gmail.com>
2016-02-04 17:57:17 -0800

    Displays boundary polygon

--- disasterinfosite/static/js/app.js
+++ disasterinfosite/static/js/app.js
@@ -30,8 +30,8 @@ $( document ).ready(function() {
   var layer = new L.TileLayer(osmUrl, {attribution: osmAttrib}).addTo(map);
   layer.setOpacity(0.6);
   
-  var boundary = new L.GeoJSON.AJAX("static/img/boundary.geojson").addTo(map);
-
+  var boundaryPolygon = new L.GeoJSON.AJAX("static/img/boundary.geojson").addTo(map);
+  
   document.getElementById('map').style.cursor='default';
   if (query_lat && query_lng) {
     var icon = new L.Icon.Default;
