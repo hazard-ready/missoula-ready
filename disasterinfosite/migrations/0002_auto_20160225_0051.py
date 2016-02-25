8eac66e880fab7d0e73b2e5a534f40feb398d69e
nein09 <nein09@nein09.com>
2016-02-24 20:02:23 -0500

    Make migrations actually match shapefile names

--- disasterinfosite/migrations/0002_auto_20160225_0051.py
+++ disasterinfosite/migrations/0002_auto_20160225_0051.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django.db.models.deletion
 import django.contrib.gis.db.models.fields
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='EQ_Fault_Buffer',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='EQ_Fault_Shaking',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -32,7 +32,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='EQ_Fault_Worst',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -40,31 +40,31 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='EQ_Historic_Distance',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
         ),
         migrations.CreateModel(
-            name='Fire_Burn_Potential',
+            name='Fire_Burn_Probability2',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
-                ('lookup_val', models.FloatField()),
-                ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Fire_Hist_Bound',
-            fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
         ),
         migrations.CreateModel(
-            name='Fire_Worst_Case_placeholder',
+            name='Fire_Hist_Bound2',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('lookup_val', models.IntegerField()),
+                ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Fire_Worst_Case_ph2',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -72,7 +72,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Flood_Channel_Migration_Zones',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.CharField(max_length=80)),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -80,7 +80,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Flood_FEMA_DFRIM_2015',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('femades', models.CharField(max_length=80)),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -88,7 +88,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Flood_Worst_Case',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -96,7 +96,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Landslide_placeholder2',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.FloatField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -104,7 +104,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='summerstorm',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -112,7 +112,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='winterstorm',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
                 ('lookup_val', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPolygonField(srid=4326)),
             ],
@@ -120,66 +120,66 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='snugget',
             name='EQ_Fault_Buffer_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.EQ_Fault_Buffer', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.EQ_Fault_Buffer', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='EQ_Fault_Shaking_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.EQ_Fault_Shaking', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.EQ_Fault_Shaking', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='EQ_Fault_Worst_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.EQ_Fault_Worst', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.EQ_Fault_Worst', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='EQ_Historic_Distance_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.EQ_Historic_Distance', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.EQ_Historic_Distance', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
-            name='Fire_Burn_Potential_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.Fire_Burn_Potential', related_name='+', blank=True),
+            name='Fire_Burn_Probability2_filter',
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.Fire_Burn_Probability2', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
-            name='Fire_Hist_Bound_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.Fire_Hist_Bound', related_name='+', blank=True),
+            name='Fire_Hist_Bound2_filter',
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.Fire_Hist_Bound2', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
-            name='Fire_Worst_Case_placeholder_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.Fire_Worst_Case_placeholder', related_name='+', blank=True),
+            name='Fire_Worst_Case_ph2_filter',
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.Fire_Worst_Case_ph2', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='Flood_Channel_Migration_Zones_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.Flood_Channel_Migration_Zones', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.Flood_Channel_Migration_Zones', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='Flood_FEMA_DFRIM_2015_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.Flood_FEMA_DFRIM_2015', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.Flood_FEMA_DFRIM_2015', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='Flood_Worst_Case_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.Flood_Worst_Case', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.Flood_Worst_Case', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='Landslide_placeholder2_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.Landslide_placeholder2', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.Landslide_placeholder2', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='summerstorm_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.summerstorm', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.summerstorm', null=True),
         ),
         migrations.AddField(
             model_name='snugget',
             name='winterstorm_filter',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, null=True, to='disasterinfosite.winterstorm', related_name='+', blank=True),
+            field=models.ForeignKey(blank=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='disasterinfosite.winterstorm', null=True),
         ),
     ]
