f21f874ba7a9c1751df8b42de0b81e4ba3a78fe3
nein09 <nein09@nein09.com>
2016-03-31 16:05:40 -0700

    Add login to urls

--- disasterinfosite/urls.py
+++ disasterinfosite/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.gis import admin
 from django.conf import settings
+from django.contrib.auth.views import login, logout
 
 from disasterinfosite import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.app_view, name='index'),
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^accounts/login/$',  login),
+                       url(r'^accounts/logout/$', logout)
                        )
 
 if settings.DEBUG:
