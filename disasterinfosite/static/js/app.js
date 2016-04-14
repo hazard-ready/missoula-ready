5cb60ed277690d67a2b32e0c2fbed7f1e4e99452
nein09 <nein09@nein09.com>
2016-04-13 21:18:08 -0700

    Use cookie for csrf value instead of token

--- disasterinfosite/static/js/app.js
+++ disasterinfosite/static/js/app.js
@@ -244,7 +244,22 @@ $( document ).ready(function() {
   setValueOnFocus($("#user-signup__zip"), "598");
 
   var sendAjaxAuthRequest = function(url, data, error, success) {
-    var csrftoken = $('input[name="csrfmiddlewaretoken"]').val();
+    var getCookie = function(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+            }
+          }
+      }
+      return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
     $.ajaxSetup({
       crossDomain: false,
       beforeSend: function(xhr) {
