87287bb96285594dab25bb0ad18c2a82f039c985
nein09 <nein09@nein09.com>
2016-09-29 16:30:56 -0700

    Update show snugget code to match our Django version

--- disasterinfosite/settings.py
+++ disasterinfosite/settings.py
@@ -16,12 +16,12 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 else:
-# hazardready.org is the current production server. 23.92.25.126 is its numeric address. eldang.eldan.co.uk is our demo/test server  
+# hazardready.org is the current production server. 23.92.25.126 is its numeric address. eldang.eldan.co.uk is our demo/test server
     ALLOWED_HOSTS = ['hazardready.org', '.hazardready.org', '23.92.25.126', 'eldang.eldan.co.uk']
 
 # Application definition
