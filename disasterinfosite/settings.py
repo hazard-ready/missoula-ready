d5af7d75de7421c7c288ff458aa68c9a7ac0647c
melinda <melinda@melindaminch.com>
2019-01-29 15:59:34 -0800

    Fix settings syntax

--- disasterinfosite/settings.py
+++ disasterinfosite/settings.py
@@ -19,7 +19,7 @@ DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
-    SITE_URL: 'http://127.0.0.1:8000'
+    SITE_URL= 'http://127.0.0.1:8000'
 else:
 # hazardready.org is the current production server. 23.92.25.126 is its numeric address. eldang.eldan.co.uk is our demo/test server
     ALLOWED_HOSTS = ['hazardready.org', '.hazardready.org', '23.92.25.126', 'eldang.eldan.co.uk']
