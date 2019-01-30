f3d935c2207ada46c7c23d2209ee180ffdfc3f6c
melinda <melinda@melindaminch.com>
2019-01-30 14:36:14 -0800

    Add wsgi settings that are necessary in production

--- disasterinfosite/wsgi.py
+++ disasterinfosite/wsgi.py
@@ -7,8 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "disasterinfosite.settings")
+wsgi_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(wsgi_dir, '/venv/lib/python3.5/site-packages'))
+sys.path.append(wsgi_dir)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
