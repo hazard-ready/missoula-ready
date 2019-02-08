0a5647ca4353c82fa0705139cf97161c3ba4f1ee
missoulaready <hazard@hazardready.org>
2019-02-08 14:37:54 -0800

    Hotfix for wsgi.py

--- disasterinfosite/wsgi.py
+++ disasterinfosite/wsgi.py
@@ -1,17 +1,43 @@
 """
 WSGI config for disasterinfosite project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "disasterinfosite.settings")
+
 wsgi_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(wsgi_dir, '/venv/lib/python3.5/site-packages'))
-sys.path.append(wsgi_dir)
+root_dir = os.path.dirname(wsgi_dir)
+
+sys.path.append(os.path.join(root_dir, 'venv/lib/python3.5/site-packages'))
+sys.path.append(root_dir)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
