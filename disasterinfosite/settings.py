d19b94d5f7dc2a5c2ab0741ebd8f3c84f689e8e7
melinda <melinda@melindaminch.com>
2022-06-06 16:39:13 -0700

    Turn debug off

--- disasterinfosite/settings.py
+++ disasterinfosite/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
