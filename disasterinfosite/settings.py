5e7f36d4d02e65522a7393e894e9639ca5d9a781
melinda <melinda@melindaminch.com>
2022-06-16 10:08:16 -0700

    Apply setting to whitenoise as well

--- disasterinfosite/settings.py
+++ disasterinfosite/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 
-WHITENOISE_STATIC_PREFIX='/static/'
+WHITENOISE_STATIC_PREFIX='static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = (
